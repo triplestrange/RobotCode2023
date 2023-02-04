1ed2e08127050ee812f65d3ebcb1c26dc342114a
Triple Strange DS <frc1533@ecgrobotics.org>
2023-02-03 20:01:19 -0500

    Fixed Encoder Values

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,141 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxPIDController;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Electrical;
+
+public class Arm extends SubsystemBase {
+  private final CANSparkMax shoulderJoint;
+  private final CANSparkMax elbowJoint;
+  private final CANSparkMax wristJoint;
+
+  private final AbsoluteEncoder shoulderEncoder;
+  private final AbsoluteEncoder elbowEncoder;
+  private final AbsoluteEncoder wristEncoder;
+
+  private final SparkMaxPIDController shoulderPID;
+  private final SparkMaxPIDController elbowPID;
+  private final SparkMaxPIDController wristPID;
+
+  /** Creates a new Arm. */
+  public Arm() {
+    super();
+    shoulderJoint = new CANSparkMax(Electrical.SHOULDER, MotorType.kBrushless);
+    elbowJoint = new CANSparkMax(Electrical.ELBOW, MotorType.kBrushless);
+    wristJoint = new CANSparkMax(Electrical.WRIST, MotorType.kBrushless);
+
+    // TODO: determine channel & call getCalibration() on each absolute
+    // encoder to determine offset angle
+    shoulderEncoder = new AbsoluteEncoder(0, 0);
+    elbowEncoder = new AbsoluteEncoder(0, 0);
+    wristEncoder = new AbsoluteEncoder(0, 0);
+
+    shoulderPID = shoulderJoint.getPIDController();
+    elbowPID = elbowJoint.getPIDController();
+    wristPID = wristJoint.getPIDController();
+
+    shoulderJoint.setIdleMode(IdleMode.kBrake);
+    elbowJoint.setIdleMode(IdleMode.kBrake);
+    wristJoint.setIdleMode(IdleMode.kBrake);
+
+    initializePID(shoulderPID);
+    initializePID(elbowPID);
+    initializePID(wristPID);
+
+    shoulderJoint.burnFlash();
+    elbowJoint.burnFlash();
+    wristJoint.burnFlash();
+  }
+  public void moveArm(double motorPowerShoulder, double motorPowerElbow, double motorPowerWrist)  {
+    shoulderJoint.set(motorPowerShoulder);
+    elbowJoint.set(motorPowerElbow);
+    wristJoint.set(motorPowerWrist);
+  }
+   public void setShoulder(double angle) {
+    shoulderPID.setReference(angle, ControlType.kPosition);
+   }
+    public double getShoulder() {
+    return shoulderJoint.get();
+  }
+  
+  /*
+   * Sets angle between humerus and radius according to shoulder joint angle
+   * Angle 0 = radius is on top of humerus
+   */
+  public void setElbow(double angle) {
+    elbowPID.setReference(angle, ControlType.kPosition);
+  }
+
+  public double getElbow() {
+    return elbowJoint.get();
+  }
+  /*
+   * Sets angle between metacarpals and radius
+   * Angle 0 = metacarpals is on top of radius
+   */
+  public void setWrist(double angle) {
+    wristPID.setReference(angle, ControlType.kPosition);
+  }
+
+  public double getWrist() {
+    return wristJoint.get();
+  }
+
+  public void initializePID(SparkMaxPIDController controller) {
+    int kP = 1;
+    int kI = 0; 
+    int kD = 0;
+    int kMinOutput = -1;
+    int kMaxOutput = 1;
+
+    controller.setP(kP);
+    controller.setI(kI);
+    controller.setD(kD);
+    controller.setOutputRange(kMinOutput, kMaxOutput);
+  }
+
+  /*
+   * PARAMETERS
+   *  width: horizontal distance between shoulder pivot point and where wrist pivot point should be
+   *  height: vertical distance between shoulder pivot point and where wrist pivot point should be
+   * 
+   * c = (a^2 + b^2 - 2abcosC)^(1/2)
+   * a^2 + b^2 - c^2 = 2abcosC
+   * sinA/a = sinB/b = sinC/c
+   */
+  public void setArm(double width, double height) {
+  // math checked with CAD
+  // Proof: https://imgur.com/3QSfHY5
+    double a = 38;
+    double b = 34;
+    double c = Math.hypot(width, height);
+
+    double angle1 = Math.acos((Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2))/(2 * a * b));
+    
+    // figure out how to calculate shoulder angle
+    double angle2 = Math.asin((Math.sin(angle1)/c) * b);
+    
+// from horizontal
+    double shoulderAngle = Math.atan2(height,width) + angle2;
+    setShoulder(shoulderAngle);
+
+    double elbowAngle = angle1+shoulderAngle - Math.PI;
+    setElbow(elbowAngle);
+
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -0,0 +1,271 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Electrical;
+import frc.robot.Constants.ModuleConstants;
+// import frc.robot.Constants.ModuleConstants;
+// import frc.robot.Constants.SwerveDriveConstants;
+import frc.robot.Constants.SwerveConstants;
+
+@SuppressWarnings("PMD.ExcessiveImports")
+public class SwerveDrive extends SubsystemBase {
+  public PIDController transformX = new PIDController(1, 0, 0);
+  public PIDController transformY = new PIDController(1, 0, 0);
+  public PIDController rotation = new PIDController(1,0,0);
+  // Robot swerve modules
+  private final SwerveModule m_frontLeft = new SwerveModule(Electrical.FL_DRIVE,
+      Electrical.FL_STEER,
+      ModuleConstants.FL_ENCODER,
+      SwerveConstants.frontLeftSteerEncoderReversed,
+      ModuleConstants.FL_ENC_OFFSET);
+
+  private final SwerveModule m_rearLeft = new SwerveModule(Electrical.BL_DRIVE,
+      Electrical.BL_STEER,
+      ModuleConstants.BL_ENCODER,
+      SwerveConstants.backLeftSteerEncoderReversed,
+      ModuleConstants.BL_ENC_OFFSET);
+
+  private final SwerveModule m_frontRight = new SwerveModule(Electrical.FR_DRIVE,
+      Electrical.FR_STEER,
+      ModuleConstants.FR_ENCODER,
+      SwerveConstants.frontRightSteerEncoderReversed,
+      ModuleConstants.FR_ENC_OFFSET);
+
+  private final SwerveModule m_rearRight = new SwerveModule(Electrical.BR_DRIVE,
+      Electrical.BR_STEER,
+      ModuleConstants.BR_ENCODER,
+      SwerveConstants.backRightSteerEncoderReversed,
+      ModuleConstants.BR_ENC_OFFSET);
+
+  private SwerveModuleState[] swerveModuleStates;
+  private SwerveModuleState[] initStates;
+  private double horPos;
+  public ChassisSpeeds currentMovement;
+
+  // The gyro sensor
+  public final double navXPitch()  {
+  return navX.getPitch();
+
+  }
+  private static final AHRS navX = new AHRS(SPI.Port.kMXP);
+  boolean gyroReset;
+
+  // Odometry class for tracking robot pose
+  SwerveDriveOdometry m_odometry =
+      new SwerveDriveOdometry(
+          SwerveConstants.kDriveKinematics,
+          getAngle(),
+          new SwerveModulePosition[] {
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_rearLeft.getPosition(),
+            m_rearRight.getPosition()
+          });
+
+  /**
+   * Creates a new DriveSubsystem.
+   */
+  public SwerveDrive() {
+  }
+
+  /**
+   * Returns the angle of the robot as a Rotation2d.
+   *
+   * @return The angle of the robot.
+   */
+  public Rotation2d getAngle() {
+    // Negating the angle because WPILib gyros are CW positive.
+    return Rotation2d.fromDegrees((navX.getAngle() + 180) * (SwerveConstants.kGyroReversed ? 1.0 : -1.0));
+  }
+
+  public boolean getGyroReset() {
+    return gyroReset;
+  }
+
+  public void setGyroReset(boolean gyroReset) {
+    this.gyroReset = gyroReset;
+  }
+
+  @Override
+  public void periodic() {
+    // Update the odometry in the periodic block
+    m_odometry.update(
+        getAngle(),
+        new SwerveModulePosition[] {
+          m_frontLeft.getPosition(),
+          m_frontRight.getPosition(),
+          m_rearLeft.getPosition(),
+          m_rearRight.getPosition()
+        });
+    SmartDashboard.putNumber("FLSteering", m_frontLeft.m_absoluteEncoder.getAngle());
+    SmartDashboard.putNumber("FRSteering", m_frontRight.m_absoluteEncoder.getAngle());
+    SmartDashboard.putNumber("BLSteering", m_rearLeft.m_absoluteEncoder.getAngle());
+    SmartDashboard.putNumber("BRSteering", m_rearRight.m_absoluteEncoder.getAngle());
+    SmartDashboard.putNumber("FLSteeringDeg", m_frontLeft.m_absoluteEncoder.getAngle() * 180.0 / Math.PI);
+    SmartDashboard.putNumber("FRSteeringDeg", m_frontRight.m_absoluteEncoder.getAngle() * 180.0 / Math.PI);
+    SmartDashboard.putNumber("BLSteeringDeg", m_rearLeft.m_absoluteEncoder.getAngle() * 180.0 / Math.PI);
+    SmartDashboard.putNumber("BRSteeringDeg", m_rearRight.m_absoluteEncoder.getAngle() * 180.0 / Math.PI);
+    SmartDashboard.putNumber("FLSteerNEO", m_frontLeft.m_turningEncoder.getPosition());
+    SmartDashboard.putNumber("FRSteerNEO", m_frontRight.m_turningEncoder.getPosition());
+    SmartDashboard.putNumber("BLSteerNEO", m_rearLeft.m_turningEncoder.getPosition());
+    SmartDashboard.putNumber("BRSteerNEO", m_rearRight.m_turningEncoder.getPosition());
+    SmartDashboard.putNumber("FLneo", m_frontLeft.getState().angle.getRadians());
+    SmartDashboard.putNumber("FRneo", m_frontRight.getState().angle.getRadians());
+    SmartDashboard.putNumber("BLneo", m_rearLeft.getState().angle.getRadians());
+    SmartDashboard.putNumber("BRneo", m_rearRight.getState().angle.getRadians());
+    SmartDashboard.putNumber("x", getPose().getTranslation().getX());
+    SmartDashboard.putNumber("y", getPose().getTranslation().getY());
+    SmartDashboard.putNumber("r", getPose().getRotation().getDegrees());
+    SmartDashboard.putNumber("GYRO ANGLE", navX.getAngle());
+    SmartDashboard.putNumber("TurnRate", getTurnRate());
+    SmartDashboard.putNumber("Limelight Pipeline", NetworkTableInstance.getDefault()
+    .getTable("limelight").getEntry("getpipe").getDouble(0));
+  }
+
+  /**
+   * Returns the currently-estimated pose of the robot.
+   *
+   * @return The pose.
+   */
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
+  }
+
+  
+  /**
+   * Resets the odometry to the specified pose.
+   *
+   * @param pose The pose to which to set the odometry.
+   */
+  public void resetOdometry(Pose2d pose) {
+    m_odometry.resetPosition(
+      getAngle(),
+      new SwerveModulePosition[] {
+        m_frontLeft.getPosition(),
+        m_frontRight.getPosition(),
+        m_rearLeft.getPosition(),
+        m_rearRight.getPosition()
+      },
+      pose);
+  }
+
+  /**
+   * Method to drive the robot using joystick info.
+   *
+   * @param xSpeed        Speed of the robot in the x direction (forward).
+   * @param ySpeed        Speed of the robot in the y direction (sideways).
+   * @param rot           Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the
+   *                      field.
+   */
+  @SuppressWarnings("ParameterName")
+  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+
+    swerveModuleStates = SwerveConstants.kDriveKinematics.toSwerveModuleStates(
+        fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(
+            xSpeed, ySpeed, rot, getAngle())
+            : new ChassisSpeeds(xSpeed, ySpeed, rot));
+    SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates,
+        SwerveConstants.kMaxSpeedMetersPerSecond);
+    m_frontLeft.setDesiredState(swerveModuleStates[0]);
+    m_frontRight.setDesiredState(swerveModuleStates[1]);
+    m_rearLeft.setDesiredState(swerveModuleStates[2]);
+    m_rearRight.setDesiredState(swerveModuleStates[3]);
+  }
+
+  /**
+   * Sets the swerve ModuleStates.
+   *
+   * @param desiredStates The desired SwerveModule states.
+   */
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates,
+        SwerveConstants.kMaxSpeedMetersPerSecond);
+    m_frontLeft.setDesiredState(desiredStates[0]);
+    m_frontRight.setDesiredState(desiredStates[1]);
+    m_rearLeft.setDesiredState(desiredStates[2]);
+    m_rearRight.setDesiredState(desiredStates[3]);
+  }
+
+  /**
+   * Resets the drive encoders to currently read a position of 0.
+   */
+  public void resetEncoders() {
+    m_frontLeft.resetEncoders();
+    m_rearLeft.resetEncoders();
+    m_frontRight.resetEncoders();
+    m_rearRight.resetEncoders();
+  }
+
+  /**
+   * Zeroes the heading of the robot.
+   */
+  public void zeroHeading() {
+    navX.reset();
+    gyroReset = true;
+  }
+  // Pipeline 0 is Fiducial Markers
+  public void autoAlignCube() {
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(0);
+    double[] robotPosition = NetworkTableInstance.getDefault().getTable("limelight").getEntry("targetpose_robotspace").getDoubleArray(new double[6]);
+    double xSpeed = transformX.calculate(robotPosition[0],0);
+    double ySpeed = transformY.calculate(robotPosition[1],0);
+    double rSpeed = rotation.calculate(getAngle().getRadians(), Math.PI);
+    drive(xSpeed, ySpeed, rSpeed, false);
+  }
+ public void autoAlignCone() {
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(1);
+    double[] robotPosition = NetworkTableInstance.getDefault().getTable("limelight").getEntry("targetpose_robotspace").getDoubleArray(new double[6]);
+    double xSpeed = transformX.calculate(robotPosition[0],0);
+    double ySpeed = transformY.calculate(robotPosition[1],0);
+    double rSpeed = rotation.calculate(getAngle().getRadians(), Math.PI);
+    drive(xSpeed, ySpeed, rSpeed, false);
+  }
+  /**
+   * Returns the heading of the robot.
+   *
+   * @return the robot's heading in degrees, from -180 to 180
+   */
+  public static double getHeading() {
+    double heading = Math.IEEEremainder(navX.getAngle(), 360) * (SwerveConstants.kGyroReversed ? -1.0 : 1.0);
+    SmartDashboard.putNumber("heading", heading);
+    return heading;
+  }
+
+  /**
+   * Returns the turn rate of the robot.
+   *
+   * @return The turn rate of the robot, in degrees per second
+   */
+  public double getTurnRate() {
+    return navX.getRate() * (SwerveConstants.kGyroReversed ? -1.0 : 1.0);
+  }
+
+  public void zeroWheels() {
+    m_frontLeft.resetWheel();
+    m_rearLeft.resetWheel();
+    m_frontRight.resetWheel();
+    m_rearRight.resetWheel();
+  }
+}
