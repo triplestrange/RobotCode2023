a5dc93683712aba50a0c2609438afa7c03c6094d
Triple Strange DS <frc1533@ecgrobotics.org>
2023-02-22 19:11:03 -0500

    Update OneConeLeaveCommunity.java

--- RobotCode2023/src/main/java/frc/robot/commands/AutoRoutines/Routines/OneConeLeaveCommunity.java
+++ RobotCode2023/src/main/java/frc/robot/commands/AutoRoutines/Routines/OneConeLeaveCommunity.java
@@ -1,9 +1,14 @@
 package frc.robot.commands.AutoRoutines.Routines;
 
+import java.util.HashMap;
+
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.FollowPathWithEvents;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.gameplay.automations.DriveTo;
@@ -17,9 +22,21 @@ public class OneConeLeaveCommunity extends SequentialCommandGroup{
 
     public OneConeLeaveCommunity(SwerveDrive m_Drive, Arm m_Arm)    {
 
-        PathPlannerTrajectory OneConeLeaveCommunity = PathPlanner.loadPath("OneCone, Leave Community", new PathConstraints(4, 3));
+       addCommands(new armTrajectory(Constants.armConstants.HIGH_POSITION, m_Arm));
+        // This will load the file "Example Path.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
+    PathPlannerTrajectory oneConeLeaveCommunity = PathPlanner.loadPath("OneCone, Leave Community", new PathConstraints(4, 3));
 
-        addCommands(m_Drive.followTrajectoryCommand(OneConeLeaveCommunity, true));
+    // This is just an example event map. It would be better to have a constant, global event map
+    // in your code that will be used by all path following commands.
+    HashMap<String, Command> eventMap = new HashMap<>();
+    eventMap.put("marker1", new PrintCommand("Passed marker 1"));
+    
+
+    FollowPathWithEvents command = new FollowPathWithEvents(
+    m_Drive.followTrajectoryCommand(oneConeLeaveCommunity, true),
+    oneConeLeaveCommunity.getMarkers(),
+    eventMap
+);
 
     }
     
