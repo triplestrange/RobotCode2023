33e62807bfb5c42de3c34a5a025c72fcaa3ba622
Duncan Page <dmpage318@gmail.com>
2023-03-25 16:26:56 -0400

    start shuffleboard recording during match

--- RobotCode2023/src/main/java/frc/robot/Robot.java
+++ RobotCode2023/src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -97,6 +98,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
+    if (DriverStation.isFMSAttached()) {
+      Shuffleboard.startRecording();
+    }
 
     m_autonomousCommand = choose.getSelected();
 
